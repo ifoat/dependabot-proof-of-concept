5ddb1e8b85798b9e8e45d3e13f744e891535bd84
ifoat <115171415+ifoat@users.noreply.github.com>
2026-03-05 15:35:55 +0000

    Create WeatherForecast.cs

---
+++ backend/Models/WeatherForecast.cs
@@ -0,0 +1,8 @@
+namespace backend.Models;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+    public int TemperatureC { get; set; }
+    public string? Summary { get; set; }
+}
